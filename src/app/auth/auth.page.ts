657e486d8f99e38d396ea13bb43cc0ab44669666
hydevip <paullungu00@gmail.com>
2019-05-16 15:58:18 +0200

    auth page update

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -34,9 +36,24 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
 
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    if (this.isLogin) {
+      // send a request to login server
+    } else {
+      // send a request to signup server
+    }
+  }
 
-
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
