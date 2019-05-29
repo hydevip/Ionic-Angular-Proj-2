b7371d27cbff94df3ea0b756452e6dc3c5634cfc
hydevip <paullungu00@gmail.com>
2019-05-29 14:40:21 +0200

    new logic for newBooking

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,21 +1,25 @@
 import { IonItemSliding } from '@ionic/angular';
-import { Booking } from './booking.module';
+import { Booking } from './booking.model';
 import { BookingService } from './booking.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
 
   loadedBookings: Booking[];
+  private bookingSub: Subscription;
 
   constructor(private bookinService: BookingService) { }
 
   ngOnInit() {
-    this.loadedBookings = this.bookinService.bookings;
+    this.bookingSub = this.bookinService.bookings.subscribe(bookings => {
+      this.loadedBookings = bookings;
+    });
   }
 
   onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
@@ -23,4 +27,9 @@ export class BookingsPage implements OnInit {
     // cancel booking with id offerId
   }
 
+  ngOnDestroy() {
+    if (this.bookingSub) {
+      this.bookingSub.unsubscribe();
+    }
+  }
 }
--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -3,6 +3,10 @@ import { PlacesService } from './../places.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { MenuController } from '@ionic/angular';
+import { AuthService } from 'src/app/auth/auth.service';
+import { take, map, tap, delay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-discover',
@@ -12,20 +16,41 @@ import { Subscription } from 'rxjs';
 export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
   private placesSub: Subscription;
+  private chosenFilter = 'all';
 
-  constructor(private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      if (this.chosenFilter === 'all') {
+        this.relevantPlaces = this.loadedPlaces;
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      } else {
+        this.relevantPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
+        this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      }
     });
-
+  }
+  onOpenMenu() {
+    this.menuCtrl.toggle();
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+      this.chosenFilter = 'all';
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1); this.chosenFilter = 'bookable';
+    }
   }
 
   ngOnDestroy() {
