714aaed3ebb3190c9292e69505389be31facb5bb
hydevip <paullungu00@gmail.com>
2019-06-12 15:07:37 +0200

    shared module

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-location-picker',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationPickerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  onPickLocation() {
+this.modalCtrl.create({component: MapModalComponent})
+  }
+
 
 }
