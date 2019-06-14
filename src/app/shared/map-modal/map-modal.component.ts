f1fa84b24e0b8a097cae573f3c6297dd2271d4f0
hydevip <paullungu00@gmail.com>
2019-06-14 12:53:05 +0200

    maps modal update

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -6,14 +6,58 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
+  @ViewChild('map') mapElementRef: ElementRef;
 
-  constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private Renderer: Renderer2
+  ) { }
 
-onCancel() {
-this.modalCtrl.dismiss();
-}
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.getGoogleMaps().then(googleMaps => {
+      const mapEl = this.mapElementRef.nativeElement;
+      const map = new googleMaps.Map(mapEl, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 16
+      });
+
+      googleMaps.event.addListenerOnce(map, 'idle', () => {
+        this.Renderer.addClass(mapEl, 'visible');
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  onCancel() {
+    this.modalCtrl.dismiss();
+  }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCpaENb7BUVR5BgCJ9Ek0qemB9VzpGRw8I';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available.');
+        }
+      };
+    });
+  }
 
 }
